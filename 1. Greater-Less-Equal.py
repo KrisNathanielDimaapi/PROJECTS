ca8a4d66db87ad50a6411538cbc83c1b9545c908
KrisNathanielDimaapi <113610705+KrisNathanielDimaapi@users.noreply.github.com>
2022-09-15 07:07:47 -0700

    Add files via upload
    The user enters two numbers, and the program determines whether the numbers are greater than, less than, or equal.

---
+++ 1. Greater-Less-Equal.py
@@ -0,0 +1,10 @@
+number1 = int(input("Enter the 1st Number: "))
+number2 = int(input("Enter the 2nd Number: "))
+
+if (number1 > number2):
+    print(number1, "is greater than", number2)
+elif (number1 < number2):
+    print(number1, "is less than", number2)
+elif (number1 == number2):
+    print(number1, "is equal to", number2)
+    
